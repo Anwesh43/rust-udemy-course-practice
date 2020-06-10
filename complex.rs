5ff38058663062866b8a0093b0a63a3baccb6aac
Anwesh43 <anweshthecool0@gmail.com>
2020-06-10 17:16:44 +0530

    overriding plus operator

--- complex.rs
+++ complex.rs
@@ -1,4 +1,5 @@
 use std::fmt::Display;
+use std::ops::Add;
 
 struct Complex<T> {
     re : T,
@@ -14,9 +15,22 @@ impl<T> Complex<T> where T : Display {
     }
 }
 
+impl Add for Complex<i32> {
+
+    type Output = Complex<i32>;
+
+    fn add(self, c1 : Self) -> Self::Output {
+        return Complex::new(self.re + c1.re, self.im + c1.im);
+    }
+}
+
 fn main() {
     let comp1  = Complex::new(2, 3);
     let comp2 = Complex::new(3.2, 4.3);
+    let comp3 = Complex::new(3, 4);
     comp1.to_string();
     comp2.to_string();
+    comp3.to_string();
+    let comp4 = comp1 + comp3;
+    comp4.to_string();
 }
